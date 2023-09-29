9a04a18a9e5d6bbad5f404faa75de2dedfa94475
rgerm <roland.germ@admiral.at>
2023-09-29 08:42:46 +0200

    test: replace real files with mock

--- test_c4_architecture_java.py
+++ test_c4_architecture_java.py
@@ -1,18 +1,42 @@
+from unittest import mock
+from unittest.mock import mock_open, patch
 from structurizr import Workspace
 from structurizr.model import Container, Location, Component
 from c4_architecture_java import get_parent_pom_paths, parse_maven_system
 
 def test_no_modules():
-    solution_paths = get_parent_pom_paths("C:/Users/rolandgerm/projects/lkww/DTM/dtm-bahn-faehre-cmp\\acceptance-tests")
+    with mock.patch('os.walk') as mockwalk:
+        mockwalk.return_value = [
+            ('/foo', ('bar',), ('baz',))
+        ]
+
+        solution_paths = get_parent_pom_paths("/anydir")
+
     assert len(solution_paths) == 0
 
 def test_pom_file_contains_modules():
-    solution_paths = get_parent_pom_paths("C:/Users/rolandgerm/projects/lkww/DTM/dtm-bahn-faehre-cmp")
-    assert solution_paths[0].endswith("pom.xml")
+    with mock.patch('os.walk') as mockwalk:
+        mockwalk.return_value = [
+            ('/foo', ('bar',), ('pom.xml',))
+        ]
 
-def test_parse_container_named_pom_artifactid():
-    workspace = parse_maven_system(["C:/Users/rolandgerm/projects/lkww/DTM/dtm-bahn-faehre-cmp/pom.xml"])
+        with mock.patch('builtins.open', mock.mock_open(read_data='<modules></modules>')):
+            solution_paths = get_parent_pom_paths("/anydir")
+
+    assert solution_paths[0] == "/foo\\pom.xml"
+
+@patch('builtins.open', new_callable=mock.mock_open, read_data=
+        '''
+        <project xmlns="http://maven.apache.org/POM/4.0.0">
+            <parent>
+                <artifactId>parent-id</artifactId>
+            </parent>
+            <artifactId>test-id</artifactId>
+        </project>
+        ''')
+def test_parse_container_named_pom_artifactid(fake_file):
+    workspace = parse_maven_system(["pom.xml"])
 
     for software_system in workspace.model.software_systems:
         for container in sorted(software_system.containers, key=lambda x: int(x.id)):
-            assert container.name == "dtm-bahn-faehre-cmp"
+            assert container.name == "test-id"
